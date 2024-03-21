880429b397962ec7126fcd081dc8696b08a1e83b
Dionysis Theodosis <theo_187@hotmail.com>
2024-03-21 21:17:48 +0200

    uml

--- DecoratorPattern/src/decoratorpattern/AddonDecorator.java
+++ DecoratorPattern/src/decoratorpattern/AddonDecorator.java
@@ -2,7 +2,7 @@ package decoratorpattern;
 
 //Decleration of the  decorator class
 public abstract class AddonDecorator extends Beverage {
-    
+    protected Beverage beverage;
     /*Implementation of the methods of the extendes abstract class beverage 
     and declare them as abstract because we want to implement them in another
     class*/
--- DecoratorPattern/src/decoratorpattern/CaramelAddon.java
+++ DecoratorPattern/src/decoratorpattern/CaramelAddon.java
@@ -3,8 +3,7 @@ package decoratorpattern;
 //Class that implements the decorator abstract class
 public class CaramelAddon extends AddonDecorator{
     //Declaration of an Beverage object (To make to (has-a) rellationship 
-    Beverage beverage;
-    
+        
     //Initialize the properties
     public CaramelAddon(Beverage beverage){
         this.beverage = beverage;
