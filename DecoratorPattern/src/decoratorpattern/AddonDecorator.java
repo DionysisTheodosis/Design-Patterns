dd551e8aa15b7ae0da3c034e0842e626d3129b19
Dionysis Theodosis <theo_187@hotmail.com>
2024-03-21 21:31:15 +0200

    final

--- DecoratorPattern/src/decoratorpattern/AddonDecorator.java
+++ DecoratorPattern/src/decoratorpattern/AddonDecorator.java
@@ -2,6 +2,7 @@ package decoratorpattern;
 
 //Decleration of the  decorator class
 public abstract class AddonDecorator extends Beverage {
+    //Declaration of an Beverage object (To make to (has-a) rellationship 
     protected Beverage beverage;
     /*Implementation of the methods of the extendes abstract class beverage 
     and declare them as abstract because we want to implement them in another
